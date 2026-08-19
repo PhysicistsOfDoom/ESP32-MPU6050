e9ec0da30f7f9fab5e01de0ec51f37578e36683c
Corbin Beus <corbin.beus@gmail.com>
2026-08-18 18:49:41 -0600

    Created host_viz for additional raylib visuals for a 3D Cube. The cube will orient itself relevant to the MPU

---
+++ host_viz/main.cpp
@@ -0,0 +1,41 @@
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <cstdio>
+#include <cstdint>
+
+int openSerialPort(const char *path) {
+    int fd = open(path, O_RDONLY | O_NOCTTY);
+    if (fd < 0) {
+        perror("open");
+        return -1;
+    }
+
+    termios tty{};
+    tcgetattr(fd, &tty);          // read current settings
+    cfsetispeed(&tty, B115200);   // set baud rate to match firmware
+    tty.c_cflag |= (CLOCAL | CREAD);
+    tty.c_cflag &= ~PARENB;       // no parity
+    tty.c_cflag &= ~CSTOPB;       // 1 stop bit
+    tty.c_cflag &= ~CSIZE;
+    tty.c_cflag |= CS8;           // 8 data bits
+    tty.c_lflag = 0;              // raw mode: no canonical/echo/signals
+    tty.c_iflag = 0;
+    tty.c_oflag = 0;
+    tty.c_cc[VMIN]  = 1;          // block until at least 1 byte available
+    tty.c_cc[VTIME] = 0;          
+
+    tcsetattr(fd, TCSANOW, &tty);
+    return fd;
+}
+
+
+int main() {
+	int fd = openSerialPort("/dev/ttyUSB0");
+	if (fd < 0){
+		return 1;
+	}
+
+
+	return 0;
+}
